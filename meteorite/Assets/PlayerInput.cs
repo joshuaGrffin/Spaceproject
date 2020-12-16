72a65f14232b4f630c32e6631f21d850bf0a2bc8
joshuaGrffin <49694568+joshuaGrffin@users.noreply.github.com>
2020-12-15 22:47:46 -0500

    Created the Player script for movement and inputs along with ship defs

---
+++ meteorite/Assets/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    int playerNum;
+
+	public PlayerInput(int aPlayerNum)
+	{
+		playerNum = aPlayerNum;
+	}
+
+	public Vector2 GetInput_LeftStick()
+	{
+		return new Vector2(Input.GetAxis("Player" + playerNum + "Horizontal"), Input.GetAxis("Player" + playerNum + "Vertical"));
+	}
+
+	public Vector2 GetInput_RightStick()
+	{
+		return new Vector2(Input.GetAxis("Player" + playerNum + "HorizontalR"), Input.GetAxis("Player" + playerNum + "VerticalR"));
+	}
+
+	public float GetFire()
+	{
+		return Input.GetAxis("Player" + playerNum + "RightTrigger");
+	}
+
+	public float GetShield()
+	{
+		return Input.GetAxis("Player" + playerNum + "LeftTrigger");
+	}
+}
---
+++ meteorite/Assets/Ship.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "ShipDef", menuName = "CreateShip/ShipDef")]
+public class Ship : ScriptableObject
+{
+    public Mesh mesh;
+    public Material material;
+    //Weapons here (max 3?) 
+    #region stats
+    public float health;
+    public float mass;
+    public float thrustForce;
+    public float shieldPower;
+    public float rechargeRate;
+    #endregion 
+}
